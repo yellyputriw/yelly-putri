f1b595f3defe912f673e33d67f136e9cca3459f8
Yelly Putri Wahyu Ningsih <yellyputriw@gmail.com>
2022-03-30 20:50:04 +0700

    :bento: Change tag i to svg for icon

--- src/views/Abilities.js
+++ src/views/Abilities.js
@@ -2,20 +2,11 @@ import Ability from '@utils/Ability';
 
 const ability1 = new Ability('/images/about/web-design.svg', 'Web Design');
 
-const ability2 = new Ability(
-  '/images/about/web-development.svg',
-  'Web Development',
-);
+const ability2 = new Ability('/images/about/web-development.svg', 'Web Development');
 
-const ability3 = new Ability(
-  '/images/about/responsive-layout-design.svg',
-  'Responsive Layout Design',
-);
+const ability3 = new Ability('/images/about/responsive-layout-design.svg', 'Responsive Layout Design');
 
-const ability4 = new Ability(
-  '/images/about/wireframe.svg',
-  'Wireframe & Prototyping',
-);
+const ability4 = new Ability('/images/about/wireframe.svg', 'Wireframe & Prototyping');
 
 const abilities = [ability1, ability2, ability3, ability4];
 
@@ -27,9 +18,9 @@ abilities.map(({ image, title }) => {
       <div class="card m-2 border-0 card-shadow about-card" >
         <div class="card-body align-items-center">
           <img class="card-img-top" src="${image}" alt="${title}">
-          <h5 class="card-title text-center fs-5 pb-3 px-3">
+          <h3 class="card-title text-center fs-5 pb-3 px-3">
             ${title}
-          </h5>
+          </h3>
         </div>
       </div>
     </div>
