8ff32c4b186d5be1a6a6283c18b0e97ddb5bd400
Yelly Putri Wahyu Ningsih <yellyputriw@gmail.com>
2022-03-30 20:46:10 +0700

    :wrench: Update configuration host

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   TITLE: 'Yelly Putri | Portfolio',
-  DESCRIPTION:
-    'Yelly Putri is a developer who specializes in designing and building exceptional digital experiences.',
-  HOST: 'http://localhost:3000',
+  DESCRIPTION: 'Yelly Putri is a developer who specializes in designing and building exceptional digital experiences.',
+  HOST: 'https://yelly.netlify.app',
 };
