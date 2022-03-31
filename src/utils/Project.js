5496b2dfbbfa1af5b1927135d2d37013f564acc7
Yelly Putri Wahyu Ningsih <yellyputriw@gmail.com>
2022-03-31 09:17:16 +0700

    :truck: Update link href

--- src/utils/Project.js
+++ src/utils/Project.js
@@ -5,14 +5,22 @@ class Project {
    * @param {string} title
    * @param {string} description
    * @param {string} link
+   * @param {string} fade
    * @returns
    */
-  static add(image, title, description, link = '#') {
+  static add(
+    image,
+    title,
+    description,
+    link = '#',
+    fade
+  ) {
     return {
       image,
       title,
       description,
       link,
+      fade,
     };
   }
 }
